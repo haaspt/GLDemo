90cb5c50b35d0968227d357617b994cb0e1af751
Tyler Haas <tyler.haas@innovation.ca.gov>
2025-11-18 16:40:48 -0800

    Add basic movement controls for the light

--- src/game/LightSource.hpp
+++ src/game/LightSource.hpp
@@ -15,6 +15,11 @@ class LightSource : public GameObject {
 public:
     LightSource() : GameObject("pyramid", "light_source") {}
 
-    void process(double const /*delta_t*/) override {}
+    void process(double const delta_t) override {
+        Vector3 input_vec = Input::get_input_vec();
+        position.x += input_vec.x * delta_t * -1.0;
+        position.y += input_vec.y * delta_t * 1.0;
+        position.z += input_vec.z * delta_t * 1.0;
+    }
 
 };
