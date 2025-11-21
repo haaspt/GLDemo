9328f38e296fe32b4ed248794ecf15b866bb9b7b
Tyler Haas <tyler.haas@innovation.ca.gov>
2025-11-21 10:51:38 -0800

    Remove old warp speed code

--- src/game/Cube.hpp
+++ src/game/Cube.hpp
@@ -11,17 +11,6 @@ class Cube : public GameObject {
 public:
     Cube() : GameObject("cube", "default", "default.jpg") {}
 
-    const double max_warp_speed = 1000;
-    const double max_lateral_speed = 450;
-
-    double warp_accel = 400;
-    double lateral_accel = 400;
-    double lateral_damping = 800;
-
-    double warp_speed = 600;
-
-    Vector3 accel_vector{0.0};
-
     void process(double const delta_t) override {rotation_rad.y += delta_t;}
 
 };
--- src/game/Pyramid.hpp
+++ src/game/Pyramid.hpp
@@ -14,18 +14,7 @@
 class Pyramid : public GameObject {
 public:
     Pyramid() : GameObject("pyramid", "default") {}
-
-    const double max_warp_speed = 1000;
-    const double max_lateral_speed = 450;
-
-    double warp_accel = 400;
-    double lateral_accel = 400;
-    double lateral_damping = 800;
-
-    double warp_speed = 600;
-
-    Vector3 accel_vector{0.0};
-
+    
     void process(double const delta_t) override;
 
 };
