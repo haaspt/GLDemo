27f83c526ee64ca54c148f3f697d798f90728ace
Tyler Haas <tyler.haas@innovation.ca.gov>
2025-11-24 14:35:18 -0800

    Implement mouse movement tracking and capture

--- src/utilities/Input.cpp
+++ src/utilities/Input.cpp
@@ -7,6 +7,11 @@
 bool Input::initialized = false;
 GLFWwindow* Input::window;
 
+bool Input::cursor_captured = false;
+
+Vector2 Input::last_cursor_pos_ = Vector2{0};
+Vector2 Input::cursor_move_vec_ = Vector2{0};
+
 Vector3 Input::get_input_vec() {
     if (!initialized) {
         throw std::runtime_error("Input not initialized! Call Input::initialize() first.");
@@ -37,4 +42,55 @@ Vector3 Input::get_input_vec() {
         input_vector.z -= 1;  // Back
     }
     return input_vector;
+}
+
+void Input::poll() {
+    update_cursor_move_vec_();
+}
+
+void Input::capture_cursor() {
+    if (!cursor_captured) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        if (glfwRawMouseMotionSupported()) {
+            glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+        }
+        cursor_captured = true;
+
+        // Reset stored cursor pose
+        glfwGetCursorPos(window, &last_cursor_pos_.x, &last_cursor_pos_.y);
+    }
+}
+
+void Input::release_cursor() {
+    if (cursor_captured) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        if (glfwRawMouseMotionSupported()) {
+            glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+        }
+        cursor_captured = false;
+
+        // Reset stored cursor pose
+        glfwGetCursorPos(window, &last_cursor_pos_.x, &last_cursor_pos_.y);
+    }
+}
+
+void Input::keyboard_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        release_cursor();
+    }
+}
+
+void Input::mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        capture_cursor();
+    }
+}
+
+
+void Input::update_cursor_move_vec_() {
+    if (!cursor_captured) return;
+    Vector2 current_cursor_pos;
+    glfwGetCursorPos(window, &current_cursor_pos.x, &current_cursor_pos.y);
+    cursor_move_vec_ = current_cursor_pos - last_cursor_pos_;
+    last_cursor_pos_ = current_cursor_pos;
 }
--- src/utilities/Input.hpp
+++ src/utilities/Input.hpp
@@ -14,11 +14,35 @@ public:
     static void initialize(GLFWwindow* glfw_window) {
         Input::window = glfw_window;
         initialized = true;
+
+        capture_cursor();
+
+        glfwSetCursorPosCallback(window, cursor_position_callback);
+        glfwSetMouseButtonCallback(window, mouse_button_callback);
+        glfwSetKeyCallback(window, keyboard_callback);
     }
 
     static Vector3 get_input_vec();
+    static Vector2 get_cursor_vec() { return cursor_move_vec_; }
+
+    static void poll();
+
+    static void capture_cursor();
+    static void release_cursor();
+    static bool is_cursor_captured() { return cursor_captured; }
+
 
 private:
     static bool initialized;
     static GLFWwindow* window;
+
+    static bool cursor_captured;
+
+    static Vector2 last_cursor_pos_;
+    static Vector2 cursor_move_vec_;
+
+    static void cursor_position_callback(GLFWwindow* /*window*/, double /*x_pos*/, double /*y_pos*/) {}
+    static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+    static void keyboard_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+    static void update_cursor_move_vec_();
 };
