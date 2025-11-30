f7c398e0b277b6cb3f023f74195b9c71df440380
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2025-11-29 21:01:37 -0800

    Split out functions to get current vs clean transform

--- src/objects/GameObject.cpp
+++ src/objects/GameObject.cpp
@@ -6,7 +6,7 @@
 
 void GameObject::render(const Camera* camera, const std::vector<const LightSource*>& lights) const {
     shader->use();
-    shader->set_mat4("model", get_transform().to_glm());
+    shader->set_mat4("model", get_clean_transform().to_glm());
     shader->set_mat4("view", camera->get_view_matrix().to_glm());
     shader->set_mat4("projection", camera->get_projection_matrix().to_glm());
     shader->set_vec3("view_pos", camera->get_position().to_glm());
