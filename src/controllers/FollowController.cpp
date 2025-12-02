e37c5d8dadb851e239389b79b00beb5a041a5bdd
Tyler Haas <tyler.haas@innovation.ca.gov>
2025-12-01 16:59:54 -0800

    Re-implement to follow object orientation

--- src/controllers/FollowController.cpp
+++ src/controllers/FollowController.cpp
@@ -6,10 +6,9 @@
 
 void FollowController::update(Node& node, double delta_t) {
     if (anchor_node_) {
-        Vector3 anchor_pos = anchor_node_->get_global_position();
-        Vector3 target_pos = anchor_pos + offset_;
-        Vector3 node_global_pos = node.get_global_position();
-        Vector3 delta_pos = target_pos - node_global_pos ;
-        node.set_position(node.get_position() + delta_pos);
+        Transform anchor_trans = anchor_node_->get_global_transform();
+        anchor_trans.translate(offset_);
+        node.set_position(anchor_trans.get_translation());
+        node.set_rotation(anchor_trans.get_rotation());
     }
 }
