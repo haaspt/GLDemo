b623229663bd6c6f9138002d6c97ba2849d2fc49
Tyler Haas <tyler.haas@innovation.ca.gov>
2025-12-01 16:57:37 -0800

    Fix light rendering bug

--- src/objects/GameObject.cpp
+++ src/objects/GameObject.cpp
@@ -11,7 +11,7 @@ void GameObject::render(const Camera* camera, const std::vector<const LightSourc
     shader->set_mat4("projection", camera->get_projection_matrix().to_glm());
     shader->set_vec3("view_pos", camera->get_position().to_glm());
     for (const auto& light : lights) {
-        shader->set_vec3("light_pos", light->get_position().to_glm());
+        shader->set_vec3("light_pos", light->get_global_position().to_glm());
         shader->set_vec3("light_color", light->get_color().to_glm());
         shader->set_float("ambient_strength", light->get_strength());
     }
